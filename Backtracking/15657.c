af04fa3e6e50a7bb4a1224e3d86d6516049ab231
Yoonseop Lee <navy10021@snu.ac.kr>
2023-06-28 17:27:19 +0900

    Create 15657.c

---
+++ Backtracking/15657.c
@@ -0,0 +1,35 @@
+// N개의 자연수(다른 수)와 자연수 M이 주어졌을 때 길이가 M인 수열 + 사전순으로 증가 + 같은 수 여러번 가능 
+#include <bits/stdc++.h>
+using namespace std;
+
+int N, M;
+int arr[10];
+int num[10];
+
+void func(int k){
+    if (k == M){
+        for(int i=0; i< M; i++)
+            cout << num[arr[i]] << ' ';
+        cout << '\n';
+        return;
+    }
+    // k=0 일때 시작점 0 
+    int st = 0;
+    // k!=0 일때 시작점 arr[k-1]
+    if (k != 0) st = arr[k-1];
+    
+    
+    for (int i = st; i < N; i++){
+        arr[k] = i;
+        func(k+1);
+    }
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> N >> M;
+    for (int i=0; i < N; i++) cin >> num[i];
+    sort(num, num + N); // 정렬
+    func(0);
+}
