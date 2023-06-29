411ac3fc2799860cbeaee5bf130316d588274651
Yoonseop Lee <navy10021@snu.ac.kr>
2023-06-29 10:36:28 +0900

    Create 6603.c

---
+++ Backtracking/6603.c
@@ -0,0 +1,32 @@
+// 로또
+// 49가지 수중 6개의 수를 골라 집합을 만든다.
+
+#include <bits/stdc++.h>
+using namespace std;
+
+int k, input[20], arr[10];
+
+void func(int cnt, int cur){
+    if (cnt == 6){
+        for (int i = 0; i < 6; i++)
+            cout << arr[i] << ' ';
+        cout << '\n';
+        return;
+    }
+    for (int i = cur; i < k; i++){
+        arr[cnt] = input[i];
+        func(cnt + 1, i + 1);
+    }
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    while (1){
+        cin >> k;
+        if (k == 0) break;
+        for (int i = 0; i < k; i++) cin >> input[i];
+        func(0, 0);
+        cout << '\n';
+    }
+}
