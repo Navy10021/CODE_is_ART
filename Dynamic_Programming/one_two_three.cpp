287a49d8223a9c0cd9b4f53e336756d8ab83b96e
Yoonseop Lee <navy10021@snu.ac.kr>
2023-07-04 14:42:08 +0900

    Create one_two_three.cpp

---
+++ Dynamic_Programming/one_two_three.cpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// 1, 2, 3 더하기 : 정수 n이 주어졌을 떄 1, 2, 3의 합으로 나타내는 방법의 수.
+
+int DP[20];
+int main(void){
+    int n;
+    cin >> n;
+    // base condition
+    DP[1] = 1; DP[2] = 2; DP[3] = 4;
+    
+    // n : 11 보다 작은 정수 
+    for (int i=4; i<11; i++){
+        DP[i] = DP[i-1] + DP[i-2] + DP[i-3];
+    }
+    cout << DP[n] << '\n';
+}
