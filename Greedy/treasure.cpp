eae8455a3bb3ca2f8f14226af1a9346cd549ce70
Yoonseop Lee <navy10021@snu.ac.kr>
2023-07-08 21:19:19 +0900

    Update treasure.cpp

--- Greedy/treasure.cpp
+++ Greedy/treasure.cpp
@@ -16,7 +16,7 @@ int main(void){
     cin.tie(0);
     cin >> n;
     for (int i=0; i<n; i++) cin >> a[i];
-    for (int i=0; i<n; j++) cin >> b[i];
+    for (int i=0; i<n; i++) cin >> b[i];
     
     sort(a, a+n);
     sort(b, b+n);
