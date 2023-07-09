a681fd8aa1442e4c1767fb6ec69bcbbfd1f576c6
Yoonseop Lee <navy10021@snu.ac.kr>
2023-07-09 21:06:50 +0900

    Create print_subseq.cpp

---
+++ string/print_subseq.cpp
@@ -0,0 +1,25 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+// print all subsequence of given string 
+
+void sub_seq(string in_str, string out_str){
+    // Base case
+    if (in_str.empty()){
+        cout << out_str << '\n';
+        return;
+    }
+    // including i-st char 
+    sub_seq(in_str.substr(1), out_str + in_str[0]);
+    // excluding i-st char 
+    sub_seq(in_str.substr(1), out_str);
+}
+
+
+int main(void){
+    string input = "ABC";
+    string output = "";
+    sub_seq(input, output);
+    
+    return 0;
+}
