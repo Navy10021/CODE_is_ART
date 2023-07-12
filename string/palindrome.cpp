8401d3138ebd9873aa67b09138502054c6a8b5ef
Yoonseop Lee <navy10021@snu.ac.kr>
2023-07-12 09:10:06 +0900

    Create palindrome.cpp

---
+++ string/palindrome.cpp
@@ -0,0 +1,36 @@
+// Palindrome
+#include <iostream>
+#include <string.h>
+using namespace std;
+
+bool palindrome(char* str){
+    int l;
+    int r = strlen(str) - 1;
+    
+    while(r > l){
+        if (str[l++] != str[r--]) 
+            return false;
+    }
+    return true;
+}
+
+bool palindrome2(string str){
+    int l;
+    int r = str.size() - 1;
+    while (r > l){
+        if (str[l++] != str[r--])
+            return false;
+    }
+    return true;
+}
+
+int main()
+{
+    //char str[] = "SNUNS";
+    //bool res = palindrome(str);
+    //cout << res << '\n';        // true = 1 ; false = 0;
+    
+    string str2 = "SNUNS";
+    bool res2 = palindrome2(str2);
+    cout << res2;
+}
