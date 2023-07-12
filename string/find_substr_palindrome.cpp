b29193ea88d4bbc3d8c62a90e25316eba79cacf4
Yoonseop Lee <navy10021@snu.ac.kr>
2023-07-12 15:26:33 +0900

    Create find_substr_palindrome.cpp

---
+++ string/find_substr_palindrome.cpp
@@ -0,0 +1,52 @@
+// Naive Pattern searching algorithm
+#include <bits/stdc++.h>
+using namespace std;
+
+
+// is palindrome;
+bool palindrome(string str){
+    int l = 0;
+    int r = str.size()-1;
+    while (r > l){
+        if (str[l++] != str[r--])
+            return false;
+    }
+    return true;
+}
+
+// Order by length
+struct myOrder{
+    bool operator() (const string& left, const string& right) const{
+        if (left.size()==right.size()){
+            return left < right;
+        }
+        else {
+            return left.size() < right.size();
+        }
+    }
+};
+
+
+void find_palindrome_substring(string str){
+    set <string, myOrder> s;
+    // Traverse string substring
+    int n = str.size();
+    for (int i = 0; i < n; i++){
+        for (int len = 1; len <= n; len ++){
+            // if substring is palindrome -> insert in set 
+            if (palindrome(str.substr(i, len)))
+                s.insert(str.substr(i, len));
+        }
+    }
+    // print set
+    for(auto iter : s) cout << iter << " ";
+}
+
+
+int main()
+{   
+    string txt = "ABCBA";
+    find_palindrome_substring(txt);
+    return 0;
+    
+}
