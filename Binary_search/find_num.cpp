72871c85886cbbf2e55338eaa2e6f8dc96f646f7
Yoonseop Lee <navy10021@snu.ac.kr>
2023-07-15 21:26:56 +0900

    Update find_num.cpp

--- Binary_search/find_num.cpp
+++ Binary_search/find_num.cpp
@@ -1,1 +1,42 @@
 // 수찾기
+#include <bits/stdc++.h>
+using namespace std;
+
+int a[100005];
+int n;          // array size 
+
+int binary_search(int target){
+    int st = 0;
+    int en = n-1;
+    
+    while (st <= en){ // end < start 이면 target이 존재하지 않는 것으로 OUT 
+        int mid = (st + en) / 2;    // min index 
+        // 1. 작은 경우 왼쪽을 날림
+        if (a[mid] < target)
+            st = mid + 1;
+        // 2. 큰 경우 오른쪽을 날림 
+        else if (a[mid] > target)
+            en = mid -1;
+        // 3. a[mid] == target : 찾은 경우  
+        else
+            return 1;   // true 
+    }
+    return 0;       // en < st 이면 while 문 탈출 
+}
+
+int main(void){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    
+    cin >> n;
+    for (int i = 0; i < n; i++) cin >> a[i];
+    sort(a, a+n);   // 정렬
+    
+    int m;
+    cin >> m;
+    while (m--){
+        int tgt;
+        cin >> tgt;
+        cout << binary_search(tgt) << '\n';
+    }
+}
