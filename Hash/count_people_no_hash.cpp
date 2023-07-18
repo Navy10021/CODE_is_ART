74f93c6d4f81e132265dfe02a5af83834a793f11
Yoonseop Lee <navy10021@snu.ac.kr>
2023-07-18 10:47:57 +0900

    Create count_people_no_hash.cpp

---
+++ Hash/count_people_no_hash.cpp
@@ -0,0 +1,24 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int n;
+string check, name;
+
+int main(void){
+    vector<string> m;
+    
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> n;
+    while (n--){
+        cin >> name >> check;
+        if (check == "enter"){
+            m.push_back(name);
+        }
+        else if (check == "leave"){
+            m.erase(remove(m.begin(), m.end(), name), m.end());
+        }
+    }
+    
+    cout << m.size();
+}
