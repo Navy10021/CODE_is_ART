56f1fe8822d811539e556ec02df0a31fac06a4c7
Yoonseop Lee <navy10021@snu.ac.kr>
2023-07-19 15:42:03 +0900

    Create dual_priority_queue_2.cpp

---
+++ Binary_search_tree/dual_priority_queue_2.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    int t;
+    cin >> t;
+    multiset<int> ms;
+    while(t--){
+        char oper;
+        int q;
+        cin >> oper >> q;
+        
+        if (oper == 'D'){
+            if (ms.empty()) continue;
+            // prev : 가장 큰 원소의 주소값 ex) prev(ms.end(), index) : i-번째 큰수 
+            if (q == 1) ms.erase(prev(ms.end()));
+            // multiset.begin() : 가장 작은 원소의 주소값 
+            else ms.erase(ms.begin());
+        }
+        // oper == I 
+        else {
+            ms.insert(q);
+        }
+    }
+    
+    if (ms.empty()) cout << "EMPTY";
+    
+    else{
+        cout << *prev(ms.end()) << ' ' << *ms.begin() << '\n';
+    }
+    
+}
