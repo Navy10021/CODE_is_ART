d73c16ab9fc7190e165416d1ee0d8896147dba00
Yoonseop Lee <navy10021@snu.ac.kr>
2023-07-20 11:06:21 +0900

    Update dual_priority_queue.cpp

--- Binary_search_tree/dual_priority_queue.cpp
+++ Binary_search_tree/dual_priority_queue.cpp
@@ -17,6 +17,7 @@ int main(){
             // prev : 가장 큰 원소의 주소값 -> prev(ms.end());  // ms.end() : {0,1,2,3,4,} (<-- it)
             if (q == 1) ms.erase(prev(ms.end()));
             // multiset.begin() : 가장 작은 원소의 주소값 -> ms.begin() : ms.begin() : {0(<--it),1,2,3,4,}
+            // 만약 ms.erase(*ms.begin()) 경우 최솟값이 여러개일때 모두 삭제함.
             else ms.erase(ms.begin());
         }
         // oper == I 
