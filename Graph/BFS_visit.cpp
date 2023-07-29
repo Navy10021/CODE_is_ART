181457ab1f15cf7c599f442c6de8eae071fa671c
Yoonseop Lee <navy10021@snu.ac.kr>
2023-07-29 17:01:26 +0900

    Create BFS_visit.cpp

---
+++ Graph/BFS_visit.cpp
@@ -0,0 +1,46 @@
+/*
+BFS in Graph 
+연결 그래프에서 순회
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int v, e;   // node, edge
+vector<int> adj[10];
+bool vis[10];
+
+void BFS(){
+    queue<int> Q;
+    // 1. 시작 노드를를 넣고, 방문표시
+    Q.push(1);      
+    vis[1] = true;
+    // 2. Queue가 빌때까지, 연결된 노드(인접리스트)을 Queue에 넣고, 방문표시 
+    while(!Q.empty()){
+        int cur = Q.front();
+        Q.pop();
+        cout << cur << ' ';     // 현재 방문노드 출력
+        for (auto nxt : adj[cur]){
+            if (vis[nxt]) continue;
+            Q.push(nxt);
+            vis[nxt] = true;
+        }
+    }
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> v >> e;
+    // Directed Graph
+    for (int i=0; i < e; i++){
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        //adj[v].push_back(u);
+    }
+    // BFS
+    BFS();
+}
+
