663eab9082f49c9849b1c6d32e8385b7f767b3c7
Yoonseop Lee <navy10021@snu.ac.kr>
2023-07-29 17:12:10 +0900

    Create BFS_distance.cpp

---
+++ Graph/BFS_distance.cpp
@@ -0,0 +1,49 @@
+/*
+BFS in Graph 
+연결 그래프에서 1번 노드와의 거리 
+*/
+#include <bits/stdc++.h>
+using namespace std;
+
+int v, e;   // node, edge
+vector<int> adj[10];
+bool dist[10];  // 거리 저장
+
+void BFS(){
+    fill(dist, dist+10, -1);    // 거리 초기화 : 방문하지 않은 곳 '-1'
+    queue<int> Q;
+    // 1. 시작 노드를를 넣고, 거리 = 0
+    Q.push(1);      
+    dist[1] = 0;
+    // 2. Queue가 빌때까지, 연결된 노드(인접리스트)을 Queue에 넣고, 방문(거리)표시 
+    while(!Q.empty()){
+        int cur = Q.front();
+        Q.pop();
+        for (auto nxt : adj[cur]){
+            if (dist[nxt] != -1) continue;
+            Q.push(nxt);
+            dist[nxt] = dist[cur] + 1;   // 거리 += 1 증가 
+        }
+    }
+}
+
+
+int main()
+{
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+    cin >> v >> e;
+    // Directed Graph
+    for (int i=0; i < e; i++){
+        int u, v;
+        cin >> u >> v;
+        adj[u].push_back(v);
+        //adj[v].push_back(u);
+    }
+    // BFS
+    BFS();
+    for(int i=0; i<10; i++){
+        cout << dist[i] << ' ';
+    }
+}
+
