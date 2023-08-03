cdba88f61174385c3e22fc785a1077d03ffd93c2
Yoonseop Lee <navy10021@snu.ac.kr>
2023-08-03 19:44:42 +0900

    Update BT_traversal.cpp

--- Tree/BT_traversal.cpp
+++ Tree/BT_traversal.cpp
@@ -8,6 +8,13 @@ using namespace std;
 int lc[9] = {0,2,4,6,0,0,0,0,0};
 int rc[9] = {0,3,5,7,0,8,0,0,0};
 
+void preorder(int cur){
+    cout << cur << ' ';
+    if (lc[cur] != 0) preorder(lc[cur]);
+    if (rc[cur] != 0) preorder(rc[cur]);
+}
+
+
 void inorder(int cur){
     if (lc[cur] != 0) preorder(lc[cur]);
     cout << cur << ' ';
@@ -15,5 +22,6 @@ void inorder(int cur){
 }
 
 int main(){
+    preorder(1);
     inorder(1);
 }
