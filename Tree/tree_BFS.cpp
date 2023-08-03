5bc929863f5bc668a29a6ed7756866670e1a3ea3
Yoonseop Lee <navy10021@snu.ac.kr>
2023-08-03 20:02:43 +0900

    Update tree_BFS.cpp

--- Tree/tree_BFS.cpp
+++ Tree/tree_BFS.cpp
@@ -27,19 +27,3 @@ void BFS(int root){
         }
     }
 }
-vector<int> adj[10];
-int p[10];          // 각 정점의 부모 번호를 저장할 P 배일 
-
-void BFS(int root){
-    queue<int> Q;
-    Q.push(root);
-    while (!Q.empty()){
-        int cur = Q.front(); Q.pop();
-        cout << cur << ' ';
-        for (int nxt : adj[cur]){
-            if (p[cur] == nxt) continue;    // nxt가 cur의 부모 확인 -> 부모인 인접값은 제외 && 자식 인접값만 추가
-            Q.push(nxt);
-            p[nxt] = cur;
-        }
-    }
-}
