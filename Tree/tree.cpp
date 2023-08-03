9537768d6100dc24d2b8962342cec0f80d62b1ea
Yoonseop Lee <navy10021@snu.ac.kr>
2023-08-03 14:24:33 +0900

    Update tree.cpp

--- Tree/tree.cpp
+++ Tree/tree.cpp
@@ -8,3 +8,19 @@ BFS 알고리즘 : Graph와 동일
 - 부모 정점을 제외하고, 나머지를 전부 Queue에 넣으면 된다. -> visited array 불필요 && 부모 노드만 알면 된다
 */
 
+vector<int> adj[10];
+int p[10];          // 각 정점의 부모 번호를 저장할 P 배일 
+
+void BFS(int root){
+    queue<int> Q;
+    Q.push(root);
+    while (!Q.empty()){
+        int cur = Q.front(); Q.pop();
+        cout << cur << ' ';
+        for (int nxt : adj[cur]){
+            if (p[cur] == nxt) continue;    // nxt가 cur의 부모 확인 -> 부모인 인접값은 제외 && 자식 인접값만 추가
+            Q.push(nxt);
+            p[nxt] = cur;
+        }
+    }
+}
