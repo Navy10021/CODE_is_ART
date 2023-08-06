5ba8d6805f83113fe1a8a98a22b4a8bb3a7d7cb3
Yoonseop Lee <navy10021@snu.ac.kr>
2023-08-06 14:32:16 +0900

    Update Linked_list.cpp

--- Linked_List/Linked_list.cpp
+++ Linked_List/Linked_list.cpp
@@ -1,138 +1,96 @@
-#include <iostream>
-#include<stdc++.h>
+#include<bits/stdc++.h>
 using namespace std;
 
-// Node Struct 구현 
-struct node {
-	int data;	// Node : 데이터값을 가지는 공간 
-	node* next; // next : 다음 데이터의 주소를 저장하는 공간 
+
+struct Node{
+    int data;
+    struct Node* next;
+    Node(int data){
+        this->data = data;
+        next = NULL;
+    }
 };
 
-// Linked List 생성 
-class LinkedList {
-private:
-	node* head;
-	node* tail;
-public:
-	LinkedList() {
-		head = NULL;
-		tail = NULL;
-	}
-	void addFrontNode(int n);	// 처음에 추가
-	void addNode(int n);		// 마지막에 추가
-	void insertNode(node* preNode, int n);	// 특정 인덱스에 추가 
-	void deleteNode(node* preNode);
-	// 첫번째 노드 가져오기 
-	node* getHead() {
-		return head;
-	}
-	void display(node* head);
-	void getCount(node* head);
-	void Reverse(node* head);
+struct LinkedList{
+    Node* head;
+    LinkedList(){
+        head = NULL;
+    }
+    void printList(){
+        struct Node* temp = head;
+        while(temp != NULL){
+            cout << temp->data << " ";
+            temp = temp->next;
+        }
+    }
+    void push(int data){
+        Node* temp = new Node(data);
+        temp->next = head;
+        head = temp;
+    }
+    void push_back(int data){
+        Node* temp = new Node(data);
+
+        if (head == NULL) head = temp;
+        else{
+            Node* last_node = head;
+            while(last_node->next != NULL){
+                last_node = last_node->next;
+            } 
+            last_node->next = temp;
+        }
+    }
+
+    void del_front(){
+        // delete linked list from beginning
+        Node* temp = head;
+        head = head->next;
+        delete temp;
+    }
+
+    void del_pop(){
+        Node* temp = head;
+        while(temp->next->next !=NULL){
+            temp = temp->next;
+        }
+        temp->next = NULL;
+    }
+
+    void del_position(int position){
+        Node* temp = head;
+        Node* prev = head;
+        for (int i = 0; i < position; i++){
+            if (i == 0 && position == 1){
+                head = head->next;
+                delete(temp);
+            }
+            else{
+                // 이전 포지션 next값을 연결 후 삭제 
+                if (i == position-1 && temp){
+                    prev->next = temp->next;
+                    delete(temp);
+                }
+                else {
+                    // temp -> next 이동 && prev 이동 
+                    prev = temp;
+                    if (prev == NULL) break; // Position > len(Linked List)
+                    temp = temp->next;
+                }
+            }
+        }
+    }
 };
 
-
-void LinkedList::addFrontNode(int n) {
-	node* temp = new node;
-	temp->data = n;
-
-	// Linked List가 비어있으면 
-	if (head == NULL) {
-		head = temp;
-		tail = temp;
-	}
-	// Linked List 데이터가 있으면 
-	else {
-		temp->next = head;
-		head = temp;
-	}
-}
-
-void LinkedList::addNode(int n) {
-	node* temp = new node;
-	temp->data = n;
-	temp->next = NULL;
-
-	if (head == NULL) {
-		head = temp;
-		tail = temp;
-	}
-	else {
-		tail->next = temp;
-		tail = temp;
-	}
-}
-
-void LinkedList::insertNode(node* prevNode, int n) {
-	node* temp = new node;
-	temp->data = n;
-
-	// 1. temp의 next를 저장 
-	temp->next = prevNode->next;
-	// 2. preNode의 next를 저장 
-	prevNode->next = temp;
-}
-
-void LinkedList::deleteNode(node* preNode) {
-	// 1. 삭제할 노드를 temp에 저장 
-	node* temp = preNode->next;
-	// 2. 삭제할 노드를 제외(prevNOde next에 next를 저장)
-	preNode->next = temp->next;
-	// 3. temp 삭제 
-	delete temp;
-}
-
-void LinkedList::display(node* head) {
-	if (head == NULL) {
-		cout << '\n';
-	}
-	else {
-		/*
-		cout << head->data << " ";
-		display(head->next);*/
-		node* temp = head;
-		while (temp != NULL) {
-			cout << temp->data << " ";
-			temp = temp->next;
-		}
-	}
-	cout << "\n";
-}
-
-void LinkedList::getCount(node* head) {
-	int cnt = 0;
-	node* cur = head;
-	while (cur != NULL) {
-		cnt++;
-		cur = cur->next;
-	}
-	cout << "len of linked list : " << cnt << '\n';
-}
-
-void LinkedList::Reverse(node* head) {
-	node* curr = head;
-	node* prev = NULL;
-	node* temp = NULL;
-
-	while (curr != NULL) {
-		temp = curr->next;
-		curr->next = prev;
-		prev = curr;
-		curr = temp;
-	}
-	head = prev;
-}
-
-
-void main() {
-	LinkedList LL;
-	LL.addNode(1);
-	LL.addNode(2);
-	LL.addNode(3);
-	LL.addNode(4);
-	LL.display(LL.getHead());
-
-	// reverse 
-	LL.Reverse(LL.getHead());
-	LL.display(LL.getHead());
+int main(){
+    LinkedList LL;
+    LL.push_back(1);
+    LL.push_back(2);
+    LL.push_back(3);
+    LL.push_back(4);
+    LL.push_back(5);
+    LL.push(0);
+    LL.printList();
+    cout << "\n";
+    LL.del_position(5);
+    LL.printList();
 }
