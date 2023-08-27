a4255664d512c48700de086487ef0421e1ab9688
Yoonseop Lee <navy10021@snu.ac.kr>
2023-08-27 11:04:42 +0900

    Create string_nums_sum.c

---
+++ C/code/string_nums_sum.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+int main(){
+   int length, sum = 0;
+   char nums[100];
+
+   scanf("%d", &length);
+   scanf("%s", nums);
+
+   for (int i = 0; i < length; i++){
+    sum += nums[i] - '0';
+   }
+   
+   printf("%d\n", sum);
+
+   return 0;
+}
