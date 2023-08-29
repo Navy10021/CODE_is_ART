606679619327da3b0c859b53dfb89d2f86403cf8
Yoonseop Lee <navy10021@snu.ac.kr>
2023-08-29 20:54:15 +0900

    Create coord_sort.c

---
+++ C/sorting/coord_sort.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+typedef struct{
+    int x;
+    int y;
+} coord;
+
+int compare(const void *first, const void *second){
+    coord A = *(coord *) first;
+    coord B = *(coord *) second;
+    if(A.x > B.x)
+        return 1;
+    else if (A.x == B.x){
+        if (A.x > B.y)
+            return 1;
+        else
+            return -1;
+    }
+    return -1;
+}
+
+int main(){
+    int N;
+    scanf("%d", &N);
+    coord *arr = (coord *) calloc(N, sizeof(coord));
+    //coord arr[N];
+    for (int i=0; i<N; i++){
+        scanf("%d %d", &arr[i].x, &arr[i].y);
+    }
+    qsort(arr, N, sizeof(coord), compare);
+
+    printf("====Sorted====\n");
+    for (int i=0; i<N; i++){
+        printf("%d %d\n", arr[i].x, arr[i].y);
+    }
+}
