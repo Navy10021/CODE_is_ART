f3d5b04b3298df6f42429f2c58a8589c5e72798b
Yoonseop Lee <navy10021@snu.ac.kr>
2023-08-30 09:48:19 +0900

    Update queue.cpp

--- c++/STL/queue.cpp
+++ c++/STL/queue.cpp
@@ -1,3 +1,32 @@
 /*
 Queue : First-In-Fist-Out(FIFO)
 */
+#include <iostream> 
+#include <queue> 
+using namespace std;
+
+int main(){
+//int자료형을 저장하는 큐 생성 
+queue<int> q;
+
+//원소 삽입
+q.push(1);
+q.push(2);
+q.push(3);
+q.push(4);
+q.push(5);
+
+//맨 앞 원소 값 출력
+printf("Front : %d\n", q.front());
+
+//맨 앞 원소 팝 
+printf("POP! \n");
+q.pop(); 
+printf("Front : %d\n", q.front());
+
+//큐가 비어있다면 1 아니면 0 
+printf("is empty? %d\n", q.empty()); 
+
+//큐에 저장되어 있는 원소의 수 출력 
+printf("Queue size : %d\n", q.size()); return 0;
+}
