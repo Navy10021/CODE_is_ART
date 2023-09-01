3bcf621cf5f4e7f311b8c2a3cc096a26ce94f1cc
Yoonseop Lee <navy10021@snu.ac.kr>
2023-09-01 22:33:18 +0900

    Update binomial_coefficient.c

--- C/combinatoric/binomial_coefficient.c
+++ C/combinatoric/binomial_coefficient.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 // 팩토리얼 계산 함수
-unsigned long long factorial(int n) {
+double factorial(int n) {
     if (n == 0 || n == 1) {
         return 1;
     }
@@ -9,7 +9,7 @@ unsigned long long factorial(int n) {
 }
 
 // 이항 계수 계산 함수
-unsigned long long binomialCoefficient(int n, int k) {
+double binomialCoefficient(int n, int k) {
     if (k < 0 || k > n) {
         return 0; // 유효하지 않은 경우
     }
@@ -20,10 +20,9 @@ int main() {
     int n, k;
     scanf("%d %d", &n, &k);
 
-    unsigned long long result = binomialCoefficient(n, k);
+    double result = binomialCoefficient(n, k);
 
-    printf("C(%d, %d) = %llu\n", n, k, result);
+    printf("C(%d, %d) = %lf\n", n, k, result);
 
     return 0;
 }
-
