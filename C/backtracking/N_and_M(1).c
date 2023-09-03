5b1929891ebb9cd28796fa96c6166519572a8ae5
Yoonseop Lee <navy10021@snu.ac.kr>
2023-09-03 17:27:59 +0900

    Update N_and_M(1).c

--- C/backtracking/N_and_M(1).c
+++ C/backtracking/N_and_M(1).c
@@ -15,14 +15,13 @@ void func(int K){ // 현재까지 K개의 수를 선택.
     printf("\n");
     return;
     }
-
-    for (int i=1; i<=N; i++){ // 1~N까지의 수
+    for (int i = 1; i <= N; i++){ // 모든 arr의 index에 1 ~ N까지의 수를 사용.
         if (!is_used[i]){     // i를 사용하지 않았다면
-            arr[K] = i;       // arr에 추가하고
+            arr[K] = i;       // K번째arr에 해당수(i)를 추가하고
             is_used[i] = 1;   // 사용 표시 후
-            func(K+1);        // recursion(K+1)
+            func(K+1);        // recursion(K+1) : K+1 위치에 1~N까지의 수를 사용.
             is_used[i] = 0;   // 사용값 원위치 backtracking
-        }
+            }
     }
 }
 
@@ -31,7 +30,6 @@ int main(){
 
     func(0); // array[0] 부터 시작!
 }
-
 
 
 
