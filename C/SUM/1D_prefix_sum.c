d20544bdba9a6028875daf566d1ecf62e866cf85
Yoonseop Lee <navy10021@snu.ac.kr>
2023-09-06 20:16:03 +0900

    Create 1D_prefix_sum.c

---
+++ C/SUM/1D_prefix_sum.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main(){
+    int arr[5] = {3, 5, 7, 1, 4};
+    int psum[5];
+
+    // 1. 누적합 : psum[n] = arr[0] + arr[1] + ... arr[n]
+    psum[0] = arr[0];
+    for (int i=1; i<=5; i++){
+        psum[i] = psum[i-1] + arr[i];
+    }
+    // 2. 구간합(index : 1 ~ 3)
+    int i = 1, j = 3;
+    int res = psum[j] - psum[i-1];
+    printf("%d", res);
+}
