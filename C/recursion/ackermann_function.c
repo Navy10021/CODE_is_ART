c478a53be3cac7848afe12000790f04cff6237a8
Yoonseop Lee <navy10021@snu.ac.kr>
2023-09-14 22:31:29 +0900

    Update ackermann_function.c

--- C/recursion/ackermann_function.c
+++ C/recursion/ackermann_function.c
@@ -1,3 +1,5 @@
+#include <stdio.h>
+/*아커만 함수 A(m, n)*/
 int ackermann(int m, int n) {
     if (m == 0) {
         return n + 1;
@@ -9,4 +11,15 @@ int ackermann(int m, int n) {
         return ackermann(m - 1, ackermann(m, n - 1));
     }
     return 0; // Error case
+}
+
+int main() {
+    int m, n;
+    printf("Enter values for m and n: ");
+    scanf("%d %d", &m, &n); // (3, 2)
+
+    int result = ackermann(m, n);
+    printf("Ackermann(%d, %d) = %d\n", m, n, result);
+
+    return 0;
 }
