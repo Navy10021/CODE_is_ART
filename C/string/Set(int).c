06b4dd7a71af1d5dad1df8a4e09da1c1909a7c73
Yoonseop Lee <navy10021@snu.ac.kr>
2023-09-15 20:52:38 +0900

    Update Set(int).c

--- C/string/Set(int).c
+++ C/string/Set(int).c
@@ -3,7 +3,7 @@
 
 #define MAX_SIZE 10
 
-// Function to check if an integer is already in the set
+// Set()에 동일한 원소가 포함되어 있는지 여부 확인.
 bool contains(const int set[], int size, int value) {
     for (int i = 0; i < size; i++) {
         if (set[i] == value) {
@@ -13,7 +13,7 @@ bool contains(const int set[], int size, int value) {
     return false;
 }
 
-// Function to add an integer to the set if it's not already present
+// Set()에 원소를 추가하는 함수
 bool addToSet(int set[], int *size, int value) {
     if (*size < MAX_SIZE && !contains(set, *size, value)) {
         set[*size] = value;
