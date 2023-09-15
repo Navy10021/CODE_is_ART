197d3fb383f492b8ec3d3dde48dc95b63756b4c1
Yoonseop Lee <navy10021@snu.ac.kr>
2023-09-15 21:05:05 +0900

    Update Set(string).c

--- C/string/Set(string).c
+++ C/string/Set(string).c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 #include <stdbool.h>
-#include <string.h>
+#include <string.h> // Include the string.h header for string operations
 
 #define MAX_SIZE 10
-#define MAX_STRING_LENGTH 100
 
 // Function to check if a string is already in the set
-bool contains(const char* set[], int size, const char* value) {
+bool contains(const char *set[], int size, const char *value) {
     for (int i = 0; i < size; i++) {
         if (strcmp(set[i], value) == 0) {
             return true;
@@ -16,38 +15,56 @@ bool contains(const char* set[], int size, const char* value) {
 }
 
 // Function to add a string to the set if it's not already present
-bool addToSet(char* set[], int* size, const char* value) {
+bool insert(const char *set[], int *size, const char *value) {
     if (*size < MAX_SIZE && !contains(set, *size, value)) {
-        set[*size] = strdup(value);
-        if (set[*size] == NULL) {
-            fprintf(stderr, "Memory allocation failed\n");
-            exit(1);
-        }
+        set[*size] = value;
         (*size)++;
         return true; // Added successfully
     }
     return false; // Element not added (either set is full or element already exists)
 }
 
+// Function to erase a string from the set if it exists
+bool erase(const char *set[], int *size, const char *value) {
+    for (int i = 0; i < *size; i++) {
+        if (strcmp(set[i], value) == 0) {
+            // Shift elements to the left to remove the element at index i
+            for (int j = i; j < *size - 1; j++) {
+                set[j] = set[j + 1];
+            }
+            (*size)--;
+            return true; // Erased successfully
+        }
+    }
+    return false; // Element not found or not erased
+}
+
 int main() {
-    char* stringSet[MAX_SIZE];
+    const char *stringSet[MAX_SIZE];
     int setSize = 0;
-    char *strList[] = {"apple", "banana", "Data", "Data", "banana"};
-    // Example usage of addToSet function
-    int List_size = sizeof(strList) / sizeof(strList[0]);
-    for (int i=0; i<List_size; i++){
-        addToSet(stringSet, &setSize, strList[i]);
-    }
 
+    // Example usage of insert function
+    insert(stringSet, &setSize, "apple");
+    insert(stringSet, &setSize, "banana");
+    insert(stringSet, &setSize, "cherry");
+    insert(stringSet, &setSize, "date");
+    insert(stringSet, &setSize, "grape");
+    insert(stringSet, &setSize, "fig");
+    insert(stringSet, &setSize, "apple");
+    
     // Printing the elements in the set
-    printf("Elements in the set:\n");
+    printf("Elements in the set: ");
     for (int i = 0; i < setSize; i++) {
-        printf("%s\n", stringSet[i]);
+        printf("%s ", stringSet[i]);
     }
 
-    // Free memory for the strings
-    for (int i = 0; i < setSize; i++) {
-        free(stringSet[i]);
+    // Erase a string from the set (e.g., erase "banana")
+    const char *elementToErase = "banana";
+    bool erased = erase(stringSet, &setSize, elementToErase);
+    if (erased) {
+        printf("\nErased \"%s\" from the set.\n", elementToErase);
+    } else {
+        printf("\n\"%s\" not found or not erased from the set.\n", elementToErase);
     }
 
     return 0;
