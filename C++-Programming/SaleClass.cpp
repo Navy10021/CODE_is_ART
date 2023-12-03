53ea30750f0b993b4a3d69a073c93966b6b35209
Yoonseop Lee <navy10021@snu.ac.kr>
2023-12-03 20:53:49 +0900

    Update SaleClass.cpp

--- C++-Programming/SaleClass.cpp
+++ C++-Programming/SaleClass.cpp
@@ -8,20 +8,33 @@ class FruitSeller{
         int myMoney;
     
     public:
+        FruitSeller(int price, int num, int money)
+            : APPLE_PRICE(price), numOfApples(num), myMoney(money) {}
+        /*
         void InitMembers(int price, int num, int money){
+            if (price < 0){
+                cout << "Check apple price." << endl;
+            }
+            
+
             APPLE_PRICE = price;
             numOfApples = num;
             myMoney = money;
-        }
+        }*/
 
         int SaleApples(int money){  // 판매 가능한 사과 개수를 리턴
+            if (money < 0){
+                cout << "Information is wrong." << endl;
+                return 0;
+            }
+
             int num = money / APPLE_PRICE;
             numOfApples -= num;
             myMoney += money;
             return num;
         }
 
-        void ShowSaleRes(){
+        void ShowSaleRes() const {  // 멤버 변수로 저장된 값을 변경하지 않겠다.
             cout << "The rest apples : " << numOfApples << endl;
             cout << "Sales revenue : " << myMoney << endl << endl;
         }
@@ -33,17 +46,25 @@ class FruitBuyer{
         int numOfApples;
     
     public:
+        FruitBuyer(int money)
+            : myMoney(money), numOfApples(0) {}
+        /*
         void InitMembers(int money){
             myMoney = money;
             numOfApples = 0;
         }
+        */
 
         void BuyApples(FruitSeller &seller, int money){
+            if (money < 0){
+                cout << "Information is wrong." << endl;
+                return;
+            }
             numOfApples += seller.SaleApples(money);  // Message passing(Using other object & function)
             myMoney -= money;
         }
 
-        void ShowBuyRes(){
+        void ShowBuyRes() const{
             cout << "Balance : " << myMoney << endl;
             cout << "The number of apples : " << numOfApples << endl << endl;
         }
@@ -51,13 +72,13 @@ class FruitBuyer{
 
 
 int main(void){
-    FruitSeller seller;
-    seller.InitMembers(1000, 100, 0);   // price, number, money
+    FruitSeller seller(1000, 100, 0);
+    //seller.InitMembers(1000, 100, 0);   // price, number, money
 
-    FruitBuyer buyer_1;
-    FruitBuyer buyer_2;
-    buyer_1.InitMembers(10000);           // money
-    buyer_2.InitMembers(30000);
+    FruitBuyer buyer_1(10000);
+    FruitBuyer buyer_2(50000);
+    //buyer_1.InitMembers(10000);           // money
+    //buyer_2.InitMembers(30000);
 
     buyer_1.BuyApples(seller, 5000);
     cout << "=== Seller ===" << endl;
