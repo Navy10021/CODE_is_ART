e93a49f78892f5b428c9aaabb70c9edd6337f34e
Yoonseop Lee <navy10021@snu.ac.kr>
2023-12-07 22:46:42 +0900

    Create DFS_find_path.py

---
+++ Python-Programming/DFS_find_path.py
@@ -0,0 +1,83 @@
+class GNode:
+    def __init__(self, data, color="W", dist=-1, fin=-1, parent=None):
+        self.data = data
+        self.color = color
+        self.dist = dist
+        self.fin = fin
+        self.parent = parent
+
+    def __str__(self):
+        return "(" + self.data + ")"
+
+
+def printG(graph):
+    print("====== Graph ======")
+    for key, val in graph.items():
+        print(str(key) + " :", end=" ")
+        for u in val:
+            print(str(u), end=" ")
+        print("")
+
+
+def DFS(graph, curr, end_vertex, path):
+    global time
+    time += 1
+    curr.dist = time
+    curr.color = "G"
+    path.append(curr)
+
+    if curr == end_vertex:
+        print("Path found:", end=" ")
+        for node in path:
+            print(node.data, end=" -> ")
+        print("")
+        return True     # Indicate that a path is found
+    
+    # Lexicographical order : Sorting adj List
+    graph[curr] = sorted(graph[curr], key = lambda x: x.data)
+    # UnLexicographical order : Sorting adj List
+    #graph[curr] = sorted(graph[curr], key = lambda x: x.data, reverse=True)
+    
+    for nxt in graph[curr]:
+        if nxt.color == "W":
+            nxt.parent = curr
+            if DFS(graph, nxt, end_vertex, path):
+                return True 
+
+    curr.color = "B"
+    time += 1
+    curr.fin = time
+    path.pop()         # Essential to backtrack and explore other possible paths in Graph!
+    
+    return False       # Indicate that a path is NOT found
+
+
+def find_path(graph, start_vertex, end_vertex):
+    global time 
+    time = 0
+    path = list()
+    
+    if DFS(graph, start_vertex, end_vertex, path):
+        return path 
+    else:
+        print("No path found.")
+        return None
+
+A, B, C = GNode("A"), GNode("B"), GNode("C")
+D, E, F = GNode("D"), GNode("E"), GNode("F")
+
+# Directed edges
+G = dict()
+G[A] = [C, B]
+G[B] = [A, D]
+G[C] = [A, D]
+G[D] = [B, C, F, E]
+G[E] = [D, F]
+G[F] = [D, E]
+printG(G)
+
+start_vertex = A
+end_vertex = F
+
+find_path(G, start_vertex, end_vertex)
+#DFS(G, start_vertex, end_vertex, path)
