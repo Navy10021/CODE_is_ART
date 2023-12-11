d8e7f30474b32623e9b049230fac77faaa2d4e95
Yoonseop Lee <navy10021@snu.ac.kr>
2023-12-11 13:18:43 +0900

    DFS_find_all_visited_node_paths_with_distance.py 업데이트

--- Python-Programming/DFS_find_all_visited_node_paths_with_distance.py
+++ Python-Programming/DFS_find_all_visited_node_paths_with_distance.py
@@ -1,51 +1,60 @@
 class GNode:
-  def __init__(self, data, color="W", parent=None, dist =0):
-    self.data = data
-    self.color = color
-    self.parent = parent
-    self.dist = dist
+    def __init__(self, data, color = "W", dist = -1, fin = -1, parent = None):
+        self.data = data 
+        self.color = color 
+        self.dist = dist 
+        self.finish = fin 
+        self.parent = parent
 
-def DFS(graph, curr, visited, path, all_paths):
-  #curr.color = "G"
-  visited.add(curr.data)
-  path.append((curr.data, curr.dist))  # store data & distance
-  
-  if len(visited) == len(graph):
-    all_paths.append(path.copy())
-  else:
-    for nxt in graph[curr]:
-      if nxt.data not in visited:
-        #nxt.color = "G"    
-        nxt.parent = curr
-        nxt.dist = curr.dist + 1
-        DFS(graph, nxt, visited, path, all_paths)
-  
-  #curr.color = "W"
-  curr.dist += 1
-  visited.remove(curr.data)
-  path.pop()
-  
+time = 0 
+def DFS(graph, curr, time, visited, path, all_paths):
+    time += 1 
+    curr.dist = time
+    #curr.color = "G"
+    visited.add(curr.data)
+    path.append((curr.data, curr.dist, curr.finish))
+    
+    if len(visited) == len(graph):
+      all_paths.append(path.copy())
+    
+    else:
+      for nxt in graph[curr]:
+        if nxt.data not in visited:
+            #nxt.color = "G"
+            nxt.parent = curr
+            DFS(graph, nxt, time, visited, path, all_paths) # Recursion
+    #curr.color = "B"
+    #print(curr.data + "(Back)", end = "-> ")
+    time += 1 
+    curr.finish = time 
+    visited.remove(curr.data)
+    path.pop()    # backtracking
 
 def find_all_paths(graph):
   all_paths = []
-  
   for start in graph:
     visited = set()
     path = []
-    for node in graph:
-      node.dist = 0 
-    DFS(graph, start, visited, path, all_paths)
-  return all_paths 
-  
+    time = -1
+    DFS(graph, start, time, visited, path, all_paths)
+  return all_paths
+    
+    
+# Adj List (Undirected edges)
 A, B, C = GNode("A"), GNode("B"), GNode("C")
-D, E = GNode("D"), GNode("E")
+D, E, F = GNode("D"), GNode("E"), GNode("F")
 
-G = {
-  A : [B, C],
-  B : [A],
-  C : [A, D],
-  D : [C, E],
-  E : [D],
-}
+# Adj List (Undirected edges)
+G = dict()
+G[A] = [B, C]
+G[B] = [A, D]
+G[C] = [A, D]
+G[D] = [B, C, E, F]
+G[E] = [D, F]
+G[F] = [D, E]
 
-print(find_all_paths(G))
+paths = find_all_paths(G)
+for path in paths:
+  for node in path:
+    print(node[0], end ="->")
+  print("\n")
