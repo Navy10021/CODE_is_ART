396f4c062649c582f40f1d63a370eafa09ec8b9b
Yoonseop Lee <navy10021@snu.ac.kr>
2023-12-25 11:00:51 +0900

    Create Erase(Specific_char).c

---
+++ C-Programming/String/Erase(Specific_char).c
@@ -0,0 +1,12 @@
+// 특정 문자를 삭제하는 함수
+void remove_char(char* str, char target) {
+    int i, j = 0
+    int length = strlen(str);
+
+    for (i = 0; i < length; i++) {
+        if (str[i] != target) {
+            str[j++] = str[i];
+        }
+    }
+    str[j] = '\0';
+}
