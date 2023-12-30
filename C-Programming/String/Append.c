2362416f79c7041d7affdd7334cb0eea547b4e5b
Yoonseop Lee <navy10021@snu.ac.kr>
2023-12-30 21:17:13 +0900

    Create Append.c

---
+++ C-Programming/String/Append.c
@@ -0,0 +1,37 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void Append(char * str, char oneChar){
+    size_t length = strlen(str);
+    str[length] = oneChar;
+    str[length + 1] = '\0';
+}
+
+
+char* Append2(char * str, char oneChar){
+    size_t length = strlen(str);
+
+    // Memory allocation
+    char *new_str = (char*)malloc((length + 2) * sizeof(char));
+
+    // Copy the original string
+    strcpy(new_str, str);
+
+    // Append Char
+    new_str[length] = oneChar;
+    new_str[length + 1] = '\0';
+
+    return new_str;
+}
+
+int main(void){
+    char input[50] = "abcd";
+    Append(input, 'A');
+    printf("%s\n", input);
+
+    char* res = Append2(input, 'B');
+    printf("%s\n", res);
+    free(res);
+    return 0;
+}
