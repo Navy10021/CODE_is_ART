31502d0253aeada688fc7701461bcbb6cb8b77e9
Yoonseop Lee <navy10021@snu.ac.kr>
2023-12-30 21:26:31 +0900

    Update Insert(Specific_Postion).c

--- C-Programming/String/Insert(Specific_Postion).c
+++ C-Programming/String/Insert(Specific_Postion).c
@@ -1,7 +1,8 @@
 #include <stdio.h>
+#include <stdlib.h>
 #include <string.h>
 
-void InsertChar(char * str, int position, char target){
+void Insert(char * str, int position, char target){
     int len = strlen(str);
     
     if (position < 0 || position > len){
@@ -16,13 +17,36 @@ void InsertChar(char * str, int position, char target){
     str[position] = target;
 }
 
+char * Insert2(char * str, int position, char addChar){
+    size_t length = strlen(str);
+    
+    if (position < 0 || position > length){
+        printf("Invalid.\n");
+        return strdup(str);
+    }
+
+    char *new_str = (char*)malloc((length+2) * sizeof(char));
+    // Copy the characters before the insertion position.
+    strncpy(new_str, str, position);
+    // Insert the character
+    new_str[position] = addChar;
+    // Copy the characters after the insertion position
+    strcpy(new_str + position + 1, str + position);
+    
+    return new_str;
+}
+
 int main(){
     char myStr[20] = "ABCD";
-    int insertPos = strlen(myStr);
+    int insertPos = 1;
     char insertc = 'X';
     
-    InsertChar(myStr, insertPos, insertc);
+    Insert(myStr, insertPos, insertc);
     printf("%s\n", myStr);
+
+    char* res = Insert2(myStr, insertPos, insertc);
+    printf("%s\n", res);
+    free(res);
     
     return 0;
 }
