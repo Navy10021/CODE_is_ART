13e35ab22dc2a20beaef2a214b29ed3bfcab376d
Yoonseop Lee <navy10021@snu.ac.kr>
2023-12-31 11:48:54 +0900

    Update LinkedList(base).py

--- Python-Programming/LinkedList/LinkedList(base).py
+++ Python-Programming/LinkedList/LinkedList(base).py
@@ -1,1 +1,60 @@
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+class LinkedList:
+    def __init__(self):
+        self.head = None
+
+    def is_empty(self):
+        return self.head is None
+
+    def append(self, data):
+        new_node = Node(data)
+        if self.is_empty():
+            self.head = new_node
+            return
+        last_node = self.head
+        while last_node.next:
+            last_node = last_node.next
+        last_node.next = new_node
+
+    def prepend(self, data):
+        new_node = Node(data)
+        new_node.next = self.head
+        self.head = new_node
+
+    def delete(self, data):
+        if self.is_empty():
+            return
+
+        if self.head.data == data:
+            self.head = self.head.next
+            return
+
+        current_node = self.head
+        while current_node.next and current_node.next.data != data:
+            current_node = current_node.next
+
+        if current_node.next:
+            current_node.next = current_node.next.next
+
+    def display(self):
+        elements = []
+        current_node = self.head
+        while current_node:
+            elements.append(current_node.data)
+            current_node = current_node.next
+        print(" -> ".join(map(str, elements)))
+
+# Example usage:
+my_linked_list = LinkedList()
+my_linked_list.append(1)
+my_linked_list.append(2)
+my_linked_list.append(3)
+my_linked_list.prepend(0)
+my_linked_list.display()
+
+# Output: 0 -> 1 -> 2 -> 3
 
