a0adb0d6c0f724aae3f5cab2348b60659cbdb5f5
Yoonseop Lee <navy10021@snu.ac.kr>
2024-01-03 12:35:28 +0900

    Subsequence_Distinct(LeetCode).c 업데이트

---
+++ C-Programming/String/Subsequence_Distinct(LeetCode).c
@@ -0,0 +1,43 @@
+#include <stdio.h>
+#include <string.h>
+
+// 115. Distinct Subsequences
+
+void genSubseq(char *str, char *tgt, int index, char *subseq, int* count) {
+    if (str[index] == '\0') {
+        if (strcmp(subseq, tgt) == 0){
+            //printf("%s\n", subseq);
+            (*count)++;
+            return;
+        }
+        return;  // Need to return!
+    }
+    // Exclude the current character
+    genSubseq(str, tgt, index + 1, subseq, count);
+
+    // Include the current character
+    subseq[strlen(subseq)] = str[index];
+    subseq[strlen(subseq) + 1] = '\0';
+    genSubseq(str, tgt, index + 1, subseq, count);
+
+    // Backtrack to exclude the current character
+    subseq[strlen(subseq) - 1] = '\0';
+}
+
+int minDistinct(char * s, char * t) {
+    int count = 0;
+    char subseq[100] = "";
+    genSubseq(s, t, 0, subseq, &count);
+
+    return count;
+}
+
+int main(void) {
+    char s[100];
+    char t[100];
+    scanf("%s %s", s, t);
+    int res = minDistinct(s, t);
+    printf("%d\n", res);
+
+    return 0;
+}
