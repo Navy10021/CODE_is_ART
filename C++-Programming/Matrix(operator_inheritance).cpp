e78410b0fb8c254e9e6e90a679d3ddf873dae62f
Yoonseop Lee <navy10021@snu.ac.kr>
2024-01-15 14:49:52 +0900

    Matrix(operator_inheritance).cpp 업데이트

--- C++-Programming/Matrix(operator_inheritance).cpp
+++ C++-Programming/Matrix(operator_inheritance).cpp
@@ -122,26 +122,130 @@ class Matrix{
             }
             return Matrix(result);
         }
-        
+
+        virtual bool IsSymmetric() const{
+            if (data.size() != data[0].size()){
+                cerr << "Error : NOT a square matrix." << endl;
+                return false;
+            }
+            for (size_t i = 0; i < data.size(); i++){
+                for (size_t j = i+1; j < data[i].size(); j++){
+                    if (data[i][j] != data[j][i]){
+                        return false;
+                    }
+                }
+            }
+            return true;
+        }
 };
 
 
 // Derived Class
-class Matrix2 : public Matrix{
-    public :
-        using Matrix::Matrix;  // Inheriting constructor
+class SymmetricMatrix : public Matrix{
+    public:
+        using Matrix::Matrix; // Inheriting constructor
+        bool IsSymmetric() const override{
+            cout << "Checking symmetry for SymmetricMatrix." << endl;
+            return Matrix::IsSymmetric();
+        }
 
-        // Additional function specific to Matrix 2 class
+        void ShowMatrix() const override{
+            if(IsSymmetric()){
+                cout << "This is a Symmetric Matrix." << endl;
+                Matrix::ShowMatrix();
+            }
+            else{
+                Matrix::ShowMatrix();
+            }
+        }
+};
 
-        // override
-        void ShowMatrix() const override {
-            cout << "This is Matrix Two class" << endl;
+class IdentityMatrix : public Matrix{
+    public:
+        IdentityMatrix(size_t n) 
+            : Matrix(GenerateIdentity(n)) {}
+
+        void ShowMatrix() const override{
+            cout << "This is a Identity Matrix." << endl;
             Matrix::ShowMatrix();
         }
 
-        int DotProduct(const Matrix& other) const{
-            cout << "This is Matris Two class" << endl;
-            return Matrix::DotProduct(other);
+    private:
+        static vector<vector<int>> GenerateIdentity(size_t n){
+            vector<vector<int>> identity(n, vector<int>(n, 0));
+            for (size_t i = 0; i < n; i++){
+                identity[i][i] = 1;
+            }
+            return identity;
+        }
+};
+
+
+class DiagonalMatrix : public Matrix{
+    public:
+        DiagonalMatrix(const vector<int>& diagonal)
+            : Matrix(GenerateDiagonal(diagonal)) {}
+
+        void ShowMatrix() const override{
+            cout << "This is a Diagonal Matrix." << endl;
+            Matrix::ShowMatrix();
+        }
+
+    private:
+        static vector<vector<int>> GenerateDiagonal(const vector<int>& diagonal){
+            size_t n = diagonal.size();
+            vector<vector<int>> res(n, vector<int>(n,0));
+            for (size_t i = 0; i < n; i++){
+               res[i][i] = diagonal[i];
+            }
+            return res;
+        }
+};
+
+class UpperTriangularMatrix : public Matrix{
+    public:
+        UpperTriangularMatrix(const vector<vector<int>>& matrix)
+            : Matrix(ExtractUpper(matrix)) {}
+
+        void ShowMatrix() const override{
+            cout << "This is an Upper Triangular Matrix." << endl;
+            Matrix::ShowMatrix();
+        }
+
+    private:
+        static vector<vector<int>> ExtractUpper(const vector<vector<int>> &matrix){
+            size_t n = matrix.size();
+            vector<vector<int>> upperTri(n, vector<int>(n,0));
+            for (size_t i = 0; i < n; i++){
+                for (size_t j = i; j < n; j++){
+                    upperTri[i][j] = matrix[i][j];
+                }
+            }
+            return upperTri;
+        }
+};
+
+
+class LowerTriangularMatrix : public Matrix{
+    public:
+        LowerTriangularMatrix(const vector<vector<int>>& matrix)
+            : Matrix(ExtractLower(matrix)) {}
+
+        void ShowMatrix() const override{
+            cout << "This is an Lower Triangular Matrix." << endl;
+            Matrix::ShowMatrix();
+        }
+
+    private:
+        static vector<vector<int>> ExtractLower(const vector<vector<int>> &matrix){
+            size_t n = matrix.size();
+            vector<vector<int>> Tri(n, vector<int>(n,0));
+            for (size_t i = 0; i < n; i++){
+                for (size_t j = 0; j <= i; j++){
+                    Tri[i][j] = matrix[i][j];
+                }
+            }
+            return Tri;
         }
 };
 
@@ -149,7 +253,13 @@ int main(){
     // Example
     Matrix m1({{1, 2}, {3, 4}});
     Matrix m2({{5, 6}, {7, 8}});
-    Matrix2 m3({{1, 1}, {1, 1}});
+    SymmetricMatrix m3({{1, 1}, {1, 1}});
+    IdentityMatrix m4(2);
+    DiagonalMatrix m5({1,2,3});
+
+    UpperTriangularMatrix m6({{1,2,3},{4,5,6},{7,8,9}});
+    LowerTriangularMatrix m7({{1,2,3},{4,5,6},{7,8,9}});
+    
     Matrix res_add = m1 + m2;
     Matrix res_sub = m1 - m2;
     Matrix res_mul = m1 * m2;
@@ -161,6 +271,15 @@ int main(){
     m1.ShowMatrix();
     cout << "Matrix 3 : " << endl;
     m3.ShowMatrix();
+    cout << "Matrix 4 : " << endl;
+    m4.ShowMatrix(); 
+    cout << "Matrix 5 : " << endl;
+    m5.ShowMatrix();
+    cout << "Matrix 6 : " << endl;
+    m6.ShowMatrix();
+    cout << "Matrix 7 : " <<endl;
+    m7.ShowMatrix();
+    
     m1 += m3;
     cout << "-------" << endl;
     m1.ShowMatrix();
