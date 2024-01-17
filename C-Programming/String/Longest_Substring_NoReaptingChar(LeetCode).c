b627fd6a87fb9e7f01fd35a501546e97190ad5f9
Yoonseop Lee <navy10021@snu.ac.kr>
2024-01-17 17:30:07 +0900

    Longest_Substring_NoReaptingChar(LeetCode).c 업데이트

---
+++ C-Programming/String/Longest_Substring_NoReaptingChar(LeetCode).c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+// LeetCode 3: Longest Substring Without Repeatng Char
+int LongestSubstr(char* str) {
+    // generate subseq and check repeatng characters.
+    
+}
+
+int main(void) {
+    char input[100];
+    scanf("%s", input);
+    int res = LongestSubstr(input);
+
+    return 0;
+}
