85b130a654e3c480b15bd2752a81bffea3b123cf
Yoonseop Lee <navy10021@snu.ac.kr>
2024-01-21 21:12:50 +0900

    Longest_Valid_Parentheses.c 업데이트

---
+++ C-Programming/String/Longest_Valid_Parentheses.c
@@ -0,0 +1,54 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <ctype.h>
+
+int isValid(char *s) {
+    int len = strlen(s);
+    char stack[len];
+    int top = -1;
+
+    for (int i = 0; i < len; i++) {
+        if (s[i] == '(' || s[i] == '[' || s[i] == '{') {
+            stack[++top] = s[i];
+        } 
+        
+        else if (isalpha(s[i]) || isalnum(s[i])){
+            continue;
+        }
+        
+        else {
+            if (top == -1) {
+                return 0;  // No matching open bracket
+            }
+
+            if ((s[i] == ')' && stack[top] != '(') ||
+                (s[i] == ']' && stack[top] != '[') ||
+                (s[i] == '}' && stack[top] != '{')) {
+                return 0;  // Mismatched closing bracket
+            }
+            top--;
+        }
+    }
+    return (top == -1);  // Check if all open brackets are closed
+}
+
+int longestValidSubstring(char *s) {
+    int len = strlen(s);
+    int max_len = 0;
+
+    for (int i = 0; i < len; i++) {
+        for (int j = i + 2; j <= len; j += 2) {
+            if (isValid(&s[i]) && j - i > max_len) {
+                max_len = j - i;
+            }
+        }
+    }
+    return max_len;
+}
+
+int main() {
+    char str[] = "(({)[][]";
+    printf("The length of the longest valid substring in %s is %d\n", str, longestValidSubstring(str));
+    return 0;
+}
