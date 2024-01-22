c1128ed1658a55983d10c0fb5b2755b0e2953f40
Yoonseop Lee <navy10021@snu.ac.kr>
2024-01-22 15:31:34 +0900

    DFS_shortest_distances_and_path.py 업데이트

---
+++ Python-Programming/DFS_shortest_distances_and_path.py
@@ -0,0 +1,53 @@
+from collections import defaultdict
+
+class GNode:
+    def __init__(self, data, d = -1, c = "W"):
+        self.data = data
+        self.distance = d
+        self.color = c
+
+# Shortest Path & Distance in a Directed Acyclic Graph(DAG) 
+def shortest_path(graph, start):
+    # Topological Sort 
+    def topological(graph):
+        def DFS(graph, curr, path):
+            curr.color = "B"
+            for nxt in graph[curr]:
+                DFS(graph, nxt, path)
+            path.insert(0, curr)
+        
+        stack = []  # store topological sorting result
+        for node in graph:
+            if node.color == "W":
+                DFS(graph, node, stack)
+        return stack
+    
+    topological_order = topological(graph)
+    distances = {node.data : float('inf') for node in graph}
+    distances[start.data] = 0
+    
+    for node in topological_order:
+        for nei in graph[node]:
+            new_distance = distances[node.data] + 1 
+            if new_distance < distances[nei.data]:
+                distances[nei.data] = new_distance
+    
+    print("The shortest distance from {} : {}".format(start.data, distances))
+    
+    return distances
+
+
+A, B, C, D = GNode('A'), GNode('B'), GNode('C'), GNode('D')
+E, F, G, H = GNode('E'), GNode('F'), GNode('G'), GNode('H')
+
+Graph = {
+    A: [B, C],
+    B: [D],
+    C: [D],
+    D: [E],
+    E: [F],
+    F: []
+}
+
+startNode = A
+shortest_path(Graph, startNode)
