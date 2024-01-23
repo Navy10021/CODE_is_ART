e94a82728be840c7b76ff2395c2075dd2e2d6bfd
Yoonseop Lee <navy10021@snu.ac.kr>
2024-01-23 15:18:17 +0900

    DFS_Find_Hamiltonian_Paths_with_Backtrack.py 업데이트

---
+++ Python-Programming/DFS_Find_Hamiltonian_Paths_with_Backtrack.py
@@ -0,0 +1,44 @@
+# Hamiltonian Cycle with Backtracking
+class GNode:
+    def __init__(self, data, color = "W"):
+        self.data = data 
+        self.color = color 
+
+def is_hamiltonian_cycle(graph):
+    hamiltonian_paths = []
+    def DFS(curr, path):
+        path.append(curr.data)
+        curr.color = "B"
+        if len(path) == len(graph):
+            hamiltonian_paths.append(path.copy())
+        else: 
+            for nei in graph[curr]:
+                if nei.color == "W":
+                    DFS(nei, path)
+        
+        # backtrack for all search
+        path.pop()
+        curr.color = "W"
+    
+    for start_node in graph:
+        DFS(start_node, [])
+    
+    hamiltonian_paths = ["->".join(p) for p in hamiltonian_paths]
+    
+    print("Hamiltonian paths : {} || {}".format(len(hamiltonian_paths), hamiltonian_paths))
+    return hamiltonian_paths
+    
+    
+A, B, C, D = GNode('A'), GNode('B'), GNode('C'), GNode('D')
+E, F, G = GNode('E'), GNode('F'), GNode('G')
+
+graph = {
+    A : [B, C],
+    B : [A, D],
+    C : [A, D],
+    D : [B, C, E, F],
+    E : [D, F],
+    F : [D, E]
+}
+
+is_hamiltonian_cycle(graph)
