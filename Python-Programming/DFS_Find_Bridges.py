171717042052dd2f886ac368621b07a7b6f4894c
Yoonseop Lee <navy10021@snu.ac.kr>
2024-01-23 21:59:03 +0900

    Update DFS_Find_Bridges.py

--- Python-Programming/DFS_Find_Bridges.py
+++ Python-Programming/DFS_Find_Bridges.py
@@ -15,6 +15,7 @@ def find_bridges(graph):
         current.disc = time
         current.low = time
         time += 1
+        print("Node : {}  discovery time : {}  low-link : {}".format(current.data, current.disc, current.low))
 
         for neighbor in graph[current]:
             if neighbor.color == "W":
@@ -22,13 +23,16 @@ def find_bridges(graph):
                 DFS(neighbor)
                 
                 current.low = min(current.low, neighbor.low)
+                print("Node : {}  discovery time : {}  low-link : {}".format(current.data, current.disc, current.low))
 
-                # Case 1
+                # Case
                 if neighbor.low > current.disc:
                     bridges.add((current.data, neighbor.data))
                 
             elif neighbor != current.parent:
                 current.low = min(current.low, neighbor.disc)
+                print("Node : {}  discovery time : {}  low-link : {}".format(current.data, current.disc, current.low))
+
 
     for start_node in graph:
         if start_node.color == "W":
