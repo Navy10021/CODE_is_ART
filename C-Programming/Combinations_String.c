6ba9766807fb06e90659a5ed1087a38fea209806
Yoonseop Lee <navy10021@snu.ac.kr>
2024-01-31 16:09:24 +0900

    Combinations_String.c 업데이트

--- C-Programming/Combinations_String.c
+++ C-Programming/Combinations_String.c
@@ -8,7 +8,7 @@ int areStringsEqual(char *str1, char *str2) {
 }
 
 // Function to check if a subset is already generated
-int isSubsetGenerated(char **subsets, int subsetCount, char *curr_combination, int k) {
+int seen(char **subsets, int subsetCount, char *curr_combination, int k) {
     for (int i = 0; i < subsetCount; i++) {
         if (areStringsEqual(subsets[i], curr_combination)) {
             return 1; // Subset is already generated
@@ -17,12 +17,11 @@ int isSubsetGenerated(char **subsets, int subsetCount, char *curr_combination, i
     return 0; // Subset is not generated yet
 }
 
-void genStringCombinations(char *arr, int arrSize, int k, char *curr_combination, int index, char **subsets, int *subsetCount) {
+void genCombinations(char *arr, int arrSize, int k, char *curr_combination, int index, char **subsets, int *subsetCount) {
     if (index == k) {
         // Check for duplicates in subsets
-        if (!isSubsetGenerated(subsets, *subsetCount, curr_combination, k)) {
+        if (!seen(subsets, *subsetCount, curr_combination, k)) {
             // Print the non-duplicated subset
-            printf("[ %s ] ", curr_combination);
 
             // Add the current subset to the list of generated subsets
             subsets[*subsetCount] = malloc((strlen(curr_combination) + 1) * sizeof(char));
@@ -34,7 +33,7 @@ void genStringCombinations(char *arr, int arrSize, int k, char *curr_combination
 
     for (int i = 0; i < arrSize; i++) {
         curr_combination[index] = arr[i];
-        genStringCombinations(arr + i + 1, arrSize - i - 1, k, curr_combination, index + 1, subsets, subsetCount);
+        genCombinations(arr + i + 1, arrSize - i - 1, k, curr_combination, index + 1, subsets, subsetCount);
     }
 }
 
@@ -47,8 +46,13 @@ void stringSubsets(char *arr) {
     for (int k = 0; k < arrSize + 1; k++) {
         char *curr_combination = (char *)malloc((k + 1) * sizeof(char));
         curr_combination[k] = '\0'; // Null-terminate the string
-        genStringCombinations(arr, arrSize, k, curr_combination, 0, allSubsets, &subsetCount);
+        genCombinations(arr, arrSize, k, curr_combination, 0, allSubsets, &subsetCount);
         free(curr_combination);
+    }
+    
+    // using saved combinations
+    for (int i=0; i<subsetCount; i++){
+        printf("{%s}\n", allSubsets[i]);
     }
 
     // Free allocated memory for subsets
@@ -59,7 +63,7 @@ void stringSubsets(char *arr) {
 }
 
 int main(void) {
-    char input[] = "abc";
+    char input[] = "abbc";
     stringSubsets(input);
 
     return 0;
