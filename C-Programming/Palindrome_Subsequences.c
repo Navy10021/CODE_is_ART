6ae9c01900275311fb8a16557ba77394f89dfb82
Yoonseop Lee <navy10021@snu.ac.kr>
2024-02-02 16:12:02 +0900

    Palindrome_Subsequences.c 업데이트

--- C-Programming/Palindrome_Subsequences.c
+++ C-Programming/Palindrome_Subsequences.c
@@ -18,27 +18,55 @@ int isPalindrome(char * str){
     return 1;
 }
 
-void partition(char* str, int l, int r, char* path, int path_idx){
+int seen(char **arr, int arr_idx, char *target) {
+    for (int i = 0; i < arr_idx; i++) {
+        if (strcmp(arr[i], target) == 0) {
+            return 1;
+        }
+    }
+    return 0;
+}
+
+void partition(char* str, int l, int r, char* path, int path_idx, char** arr, int * arr_idx){
     if (l == r){
         path[path_idx] = '\0';
-        if (isPalindrome(path)){
-            printf("%s\n", path);
+        if (isPalindrome(path) && !seen(arr, *arr_idx, path)){
+            //printf("%s\n", path);
+            strcpy(arr[(*arr_idx)++], path);
         }
         return;
     }
     path[path_idx] = str[l];
-    partition(str, l + 1, r, path, path_idx + 1);
-    partition(str, l + 1, r, path, path_idx);
+    partition(str, l + 1, r, path, path_idx + 1, arr, arr_idx);
+    partition(str, l + 1, r, path, path_idx, arr, arr_idx);
+}
+
+void subsequnces(char *str){
+    int length = strlen(str);
+    char * path = (char*)malloc((length + 1) * sizeof(char));
+    
+    int arr_size = 1 << length;
+    int arr_idx = 0;
+    char **arr = (char **)malloc(arr_size * sizeof(char *));
+    for (int i = 0; i < arr_size; i++) {
+        arr[i] = (char *)malloc((length + 1) * sizeof(char));
+    }
+    
+    partition(str, 0, length, path, 0, arr, &arr_idx);
+    
+    for (int i = 0; i < arr_idx; i++) {
+        printf("%s\n", arr[i]);
+        free(arr[i]); // Free memory for each string
+    }
+    free(path);
+    free(arr);
 }
 
 int main(void){
     char input[100];
     scanf("%s", input);
+    subsequnces(input);
     
-    int length = strlen(input);
-    char * path = (char*)malloc((length + 1) * sizeof(char));
-    partition(input, 0, length, path, 0);
-    free(path);
     return 0;
 }
 
