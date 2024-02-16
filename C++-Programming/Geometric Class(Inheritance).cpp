24ea992ef18c822bf90c9deef41184e1275cdcb7
Yoonseop Lee <navy10021@snu.ac.kr>
2024-02-16 09:38:10 +0900

    Geometric Class(Inheritance).cpp 업데이트

--- C++-Programming/Geometric Class(Inheritance).cpp
+++ C++-Programming/Geometric Class(Inheritance).cpp
@@ -1,120 +1,154 @@
 #include <iostream>
 #include <cmath>
 
+using namespace std;
+
 class Point {
-protected:
-    double x;
-    double y;
+    protected: // inheritance
+        double x;
+        double y;
 
-public:
-    Point(double _x, double _y) : x(_x), y(_y) {}
+    public:
+        Point(double _x, double _y) : x(_x), y(_y) {}
 
-    virtual void display() const {
-        std::cout << "Point - X: " << x << ", Y: " << y << std::endl;
-    }
+        virtual void display() const {
+            cout << "Point - [" << x << ", " << y << "]" << endl;
+        }
+        virtual double getArea() const = 0; // pure virtual function
+        virtual double getPerimeter() const = 0; // pure virtual function
+
+        ~Point() {}
 };
 
 class Line : public Point {
-private:
-    double endX;
-    double endY;
+    private:
+        double endX;
+        double endY;
 
-public:
-    Line(double startX, double startY, double endX, double endY) : Point(startX, startY), endX(endX), endY(endY) {}
+    public:
+        Line(double startX, double startY, double endX, double endY) : Point(startX, startY), endX(endX), endY(endY) {}
 
-    void display() const override {
-        Point::display();
-        std::cout << "End Point - X: " << endX << ", Y: " << endY << std::endl;
-    }
-
-    double calculateDistance() const {
-        return std::sqrt(std::pow(endX - x, 2) + std::pow(endY - y, 2));
-    }
+        void display() const override {
+            Point::display();
+            cout << "End Point - [" << endX << ", " << endY << "]" << endl;
+        }
+        
+        double getArea() const override {
+            return 0;
+        }
+        
+        double getPerimeter() const override{
+            return getDistance();
+        }
+        
+        double getDistance() const {
+            double dx = endX - x;
+            double dy = endY - y;
+            return sqrt(dx*dx + dy*dy);
+        }
+        
+        double getSlope() const {
+            double dx = endX - x;
+            double dy = endY - y;
+            if (dx == 0){
+                return INFINITY;
+            }
+            return dy/dx;
+        }
+        ~Line() {}
 };
 
-
+// 원 
 class Circle : public Point {
-private:
-    double radius;
+    private:
+        double radius;
 
-public:
-    Circle(double centerX, double centerY, double r) : Point(centerX, centerY), radius(r) {}
+    public:
+        Circle(double centerX, double centerY, double r) : Point(centerX, centerY), radius(r) {}
 
-    void display() const override {
-        Point::display();
-        std::cout << "Radius: " << radius << std::endl;
-    }
+        void display() const override {
+            Point::display();
+            cout << "This is Circle || ";
+            cout << "Radius: " << radius << endl;
+        }
 
-    double calculateArea() const {
-        return 3.14 * radius * radius;
-    }
+        double getArea() const override {
+            return 3.14 * radius * radius;
+        }
 
-    double calculateCircumference() const {
-        return 2 * 3.14 * radius;
-    }
+        double getPerimeter() const override{
+            return 2 * 3.14 * radius;
+        }
+        ~Circle() {}
 };
 
+// 타원
 class Ellipse : public Point {
-private:
-    double semiMajorAxis;
-    double semiMinorAxis;
+    private:
+        double semiMajorAxis;
+        double semiMinorAxis;
 
-public:
-    Ellipse(double centerX, double centerY, double a, double b) : Point(centerX, centerY), semiMajorAxis(a), semiMinorAxis(b) {}
+    public:
+        Ellipse(double centerX, double centerY, double a, double b) : Point(centerX, centerY), semiMajorAxis(a), semiMinorAxis(b) {}
 
-    void display() const override {
-        Point::display();
-        std::cout << "Semi-Major Axis: " << semiMajorAxis << ", Semi-Minor Axis: " << semiMinorAxis << std::endl;
-    }
+        void display() const override {
+            Point::display();
+            cout << "This is Ellipse || ";
+            cout << "Semi-Major Axis: " << semiMajorAxis << ", Semi-Minor Axis: " << semiMinorAxis << endl;
+        }
 
-    double calculateArea() const {
-        return 3.14 * semiMajorAxis * semiMinorAxis;
-    }
+        double getArea() const override {
+            return 3.14 * semiMajorAxis * semiMinorAxis;
+        }
 
-    double calculatePerimeter() const {
-        return 2 * 3.14 * std::sqrt((semiMajorAxis * semiMajorAxis + semiMinorAxis * semiMinorAxis) / 2);
-    }
+        double getPerimeter() const override {
+            return 2 * 3.14 * sqrt((semiMajorAxis * semiMajorAxis + semiMinorAxis * semiMinorAxis) / 2);
+        }
+        ~Ellipse() {}
 };
 
 
 class Rectangle : public Point {
-private:
-    double width;
-    double height;
+    private:
+        double width;
+        double height;
 
-public:
-    Rectangle(double leftTopX, double leftTopY, double w, double h) : Point(leftTopX, leftTopY), width(w), height(h) {}
+    public:
+        Rectangle(double leftTopX, double leftTopY, double w, double h) : Point(leftTopX, leftTopY), width(w), height(h) {}
 
-    void display() const override {
-        Point::display();
-        std::cout << "Width: " << width << ", Height: " << height << std::endl;
+        void display() const override {
+            Point::display();
+            cout << "This is Rectangle || ";
+            std::cout << "Width: " << width << ", Height: " << height << std::endl;
     }
 
-    double getWidth() const {
-        return width;
+        double getWidth() const {
+            return width;
+        }
+
+        double getHeigth() const {
+            return height;
+        }
+
+        double getArea() const override {
+            return width * height;
     }
 
-    double getHeigth() const {
-        return height;
-    }
-
-    double calculateArea() const {
-        return width * height;
-    }
-
-    double calculatePerimeter() const {
-        return 2 * (width + height);
-    }
+        double getPerimeter() const override {
+            return 2 * (width + height);
+        }
+        ~Rectangle() {}
 };
 
 class Square : public Rectangle {
-public:
-    Square(double leftTopX, double leftTopY, double side) : Rectangle(leftTopX, leftTopY, side, side) {}
+    public:
+        Square(double leftTopX, double leftTopY, double side) : Rectangle(leftTopX, leftTopY, side, side) {}
 
-    void display() const override {
-        Rectangle::display();
-        std::cout << "Side: " << Rectangle::getWidth() << std::endl; // Square has equal sides, so getting width is sufficient
-    }
+        void display() const override {
+            Rectangle::display();
+            std::cout << "Side: " << Rectangle::getWidth() << endl; // Square has equal sides, so getting width is sufficient
+        }
+        // 상속의 상속은 override에 대한 추가함수 구현 불필요!
 };
 
 
@@ -128,23 +162,24 @@ public:
     Triangle(double vertex1X, double vertex1Y, double vertex2X, double vertex2Y, double vertex3X, double vertex3Y)
         : Point(vertex1X, vertex1Y), side1(0), side2(0), side3(0) {
         // Calculate the lengths of the sides
-        side1 = std::sqrt(std::pow(vertex2X - vertex1X, 2) + std::pow(vertex2Y - vertex1Y, 2));
-        side2 = std::sqrt(std::pow(vertex3X - vertex2X, 2) + std::pow(vertex3Y - vertex2Y, 2));
-        side3 = std::sqrt(std::pow(vertex1X - vertex3X, 2) + std::pow(vertex1Y - vertex3Y, 2));
+        side1 = sqrt(pow(vertex2X - vertex1X, 2) + pow(vertex2Y - vertex1Y, 2));
+        side2 = sqrt(pow(vertex3X - vertex2X, 2) + pow(vertex3Y - vertex2Y, 2));
+        side3 = sqrt(pow(vertex1X - vertex3X, 2) + pow(vertex1Y - vertex3Y, 2));
     }
 
     void display() const override {
         Point::display();
-        std::cout << "Side1: " << side1 << ", Side2: " << side2 << ", Side3: " << side3 << std::endl;
+        cout << "This is Triangle || ";
+        cout << "Side1: " << side1 << ", Side2: " << side2 << ", Side3: " << side3 << endl;
     }
 
-    double calculateArea() const {
+    double getArea() const override {
         // Using Heron's formula to calculate the area
         double s = (side1 + side2 + side3) / 2;
         return std::sqrt(s * (s - side1) * (s - side2) * (s - side3));
     }
 
-    double calculatePerimeter() const {
+    double getPerimeter() const {
         return side1 + side2 + side3;
     }
 };
@@ -167,26 +202,35 @@ public:
 
 int main() {
     Line line(0, 0, 3, 4);
-    Circle circle(2, 3, 5);
-    Rectangle rectangle(1, 1, 4, 3);
-    Square square(5, 5, 2);
-    Ellipse ellipse(8, 8, 6, 4);
-    Triangle triangle(1, 1, 4, 1, 3, 5);
-    EquilateralTriangle equilateralTriangle(8, 8, 4);
-    IsoscelesTriangle isoscelesTriangle(15, 15, 6, 8);
-
+    // 원형 컨트롤 포인터
+    Point* ptr1 = new Circle(2, 3, 5);
+    ptr1 = new Ellipse(2, 3, 6, 4);
+    // 사각형 컨트롤 포인터
+    Point* ptr2 = new Rectangle(1, 1, 4, 3);
+    ptr2 = new Square(1, 1, 5);
+    // 삼각형 컨트롤 포인터 
+    Point* ptr3 = new Triangle(4, 4, 4, 1, 3, 5);
+    ptr3 = new EquilateralTriangle(4, 4, 5);
+    ptr3 = new IsoscelesTriangle(4, 4, 6, 8);
+    
 
     line.display();
-    std::cout << "Distance: " << line.calculateDistance() << std::endl;
-
-    circle.display();
-    std::cout << "Area: " << circle.calculateArea() << std::endl;
-    std::cout << "Circumference: " << circle.calculateCircumference() << std::endl;
-
-    rectangle.display();
-    std::cout << "Area: " << rectangle.calculateArea() << std::endl;
-    std::cout << "Perimeter: " << rectangle.calculatePerimeter() << std::endl;
-
+    cout << "Distance: " << line.getPerimeter() << endl;
+    
+    ptr1->display();
+    cout << "Area:" << ptr1->getArea() << endl;
+    cout << "Perimeter: " << ptr1->getPerimeter() << endl;
+    
+    ptr2->display();
+    cout << "Area: " << ptr2->getArea() << endl;
+    cout << "Perimeter: " << ptr2->getPerimeter() << endl;
+    
+    ptr3->display();
+    cout << "Area: " << ptr3->getArea() << endl;
+    cout << "Perimeter: " << ptr3->getPerimeter() << endl;
+    
+    
+/*
     square.display();
     std::cout << "Area: " << square.calculateArea() << std::endl;
     std::cout << "Perimeter: " << square.calculatePerimeter() << std::endl;
@@ -206,6 +250,6 @@ int main() {
     isoscelesTriangle.display();
     std::cout << "Area: " << isoscelesTriangle.calculateArea() << std::endl;
     std::cout << "Perimeter: " << isoscelesTriangle.calculatePerimeter() << std::endl;
-
+*/
     return 0;
 }
