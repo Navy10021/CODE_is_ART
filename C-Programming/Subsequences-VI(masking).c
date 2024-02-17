2eba8021804051c23ee87af77ceed6df2cfe14fa
Yoonseop Lee <navy10021@snu.ac.kr>
2024-02-17 14:40:00 +0900

    C-Programming 업데이트

---
+++ C-Programming/Subsequences-VI(masking).c
@@ -0,0 +1,34 @@
+// Combination Masking given string
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void partition(char* str, int l, int r, char* path, int path_idx){
+    if (l == r){
+        path[path_idx] = '\0';
+        printf("%s\n", path);
+        return;
+    }
+    path[path_idx] = str[l];
+    // include current char str[l]
+    partition(str, l + 1, r, path, path_idx + 1);
+    // Exclude current char as Masking
+    path[path_idx] = '#';
+    partition(str, l + 1, r, path, path_idx + 1);
+}
+
+void subsequnces(char *str){
+    int length = strlen(str);
+    char * path = (char*)malloc((length + 1) * sizeof(char));
+
+    partition(str, 0, length, path, 0);
+    free(path);
+}
+
+int main(void){
+    char input[100] = "ABCD";
+    //scanf("%s", input);
+    
+    subsequnces(input);
+    return 0;
+}
