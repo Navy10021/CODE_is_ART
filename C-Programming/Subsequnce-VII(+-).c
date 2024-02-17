42c175d5e41a1186ec61e15f50949588728e4ef5
Yoonseop Lee <navy10021@snu.ac.kr>
2024-02-17 22:54:29 +0900

    Subsequnce-VII(+-).c 업데이트

--- C-Programming/Subsequnce-VII(+-).c
+++ C-Programming/Subsequnce-VII(+-).c
@@ -4,7 +4,8 @@
 
 void generateCombinations(char* str, int l, int r, char* path, int path_idx){
     if (l == r){
-        path[path_idx] = '\0';
+        path[path_idx] = '0';
+        path[path_idx + 1] = '\0';
         printf("%s\n", path);
         return;
     }
