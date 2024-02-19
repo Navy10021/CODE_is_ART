0b57a8bb2c96dac07e44e3767a3b59b657c1d9f2
Yoonseop Lee <navy10021@snu.ac.kr>
2024-02-19 10:09:53 +0900

    Subsequences-XIII(Insert_alphabets).c 업데이트

---
+++ C-Programming/Subsequences-XIII(Insert_alphabets).c
@@ -0,0 +1,58 @@
+// Insert masking : A#B#C
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+int isPalindrome(char * str){
+    int st = 0;
+    int en = strlen(str) - 1;
+    while(st < en){
+        if (str[st] != str[en]){
+            return 0;
+        }
+        st++;
+        en--;
+    }
+    return 1;
+}
+
+void partition(char* str, int l, int r, char* path, int path_idx){
+    if (l == r){
+        path[path_idx] = '\0';
+        if (isPalindrome(path)){
+            printf("%s\n", path);
+        }
+        return;
+    }
+    // include current char str[l]
+    path[path_idx] = str[l];
+    // if it's the last character, just proceed without adding masking
+    if (l == r - 1){
+        partition(str, l + 1, r, path, path_idx + 1);
+    }
+    else{
+        // Not last character, include the current char and add masking
+        partition(str, l + 1, r, path, path_idx + 1);
+        // Insert A ~ Z alphabets
+        for (char c = 'a'; c <= 'z'; c++){
+            path[path_idx + 1] = c;
+            partition(str, l + 1, r, path, path_idx + 2);
+        }
+    }
+}
+
+void subsequnces(char *str){
+    int length = strlen(str);
+    char * path = (char*)malloc((length + 1) * sizeof(char));
+
+    partition(str, 0, length, path, 0);
+    free(path);
+}
+
+int main(void){
+    char input[100] = "aaa";
+    //scanf("%s", input);
+    
+    subsequnces(input);
+    return 0;
+}
