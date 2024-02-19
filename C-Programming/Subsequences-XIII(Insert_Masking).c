0e6f18a9c27a006424c80cd9e05d0adc7e83aa79
Yoonseop Lee <navy10021@snu.ac.kr>
2024-02-19 13:23:43 +0900

    Subsequences-XIII(Insert_Masking).c 업데이트

--- C-Programming/Subsequences-XIII(Insert_Masking).c
+++ C-Programming/Subsequences-XIII(Insert_Masking).c
@@ -3,12 +3,11 @@
 #include <stdlib.h>
 #include <string.h>
 
-void partition(char* str, int l, int r, char* path, int path_idx){
+void partition(char* str, int l, int r, char* path, int path_idx, char mask){
     if (l == r){
         path[path_idx] = '\0';
         printf("%s\n", path);
         // also front masking 
-        char mask = '#';
         char* add_path = (char*)malloc((path_idx + 1) * sizeof(char));
         add_path[0] = mask;
         strcpy(add_path + 1, path);
@@ -19,18 +18,17 @@ void partition(char* str, int l, int r, char* path, int path_idx){
     // include current char str[l]
     path[path_idx] = str[l];
     // include current char and proceed to next proceed
-    partition(str, l + 1, r, path, path_idx + 1);
+    partition(str, l + 1, r, path, path_idx + 1, mask);
     // Insert masking
-    char mask = '#';
     path[path_idx + 1] = mask;
-    partition(str, l + 1, r, path, path_idx + 2);
+    partition(str, l + 1, r, path, path_idx + 2, mask);
 }
 
 void subsequnces(char *str){
     int length = strlen(str);
     char * path = (char*)malloc((length + 1) * sizeof(char));
-
-    partition(str, 0, length, path, 0);
+    char masked_val = '#';
+    partition(str, 0, length, path, 0, masked_val);
     free(path);
 }
 
