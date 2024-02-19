5dbbc804ec8f62426df0720f25471d3bc94f3edb
Yoonseop Lee <navy10021@snu.ac.kr>
2024-02-19 09:11:52 +0900

    Subsequences-XI(Insert_Masking).c 업데이트

---
+++ C-Programming/Subsequences-XI(Insert_Masking).c
@@ -0,0 +1,41 @@
+// Insert masking
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+void partition(char* str, int l, int r, char* path, int path_idx){
+    if (l == r){
+        path[path_idx] = '\0';
+        if (path[path_idx - 1] == str[r - 1]){ // filtering same as last character
+            printf("%s\n", path);
+        }
+        return;
+    }
+    path[path_idx] = str[l];
+    // include current char str[l]
+    partition(str, l + 1, r, path, path_idx + 1);
+    // Insert two more Masking
+    char masking[3] = {'*', '#', '&'};
+    for (int i = 0; i < 3; i++){
+        path[path_idx + 1] = masking[i];
+        partition(str, l + 1, r, path, path_idx + 2);
+    }
+    //path[path_idx + 1] = mask;
+    //partition(str, l + 1, r, path, path_idx + 2);
+}
+
+void subsequnces(char *str){
+    int length = strlen(str);
+    char * path = (char*)malloc((length + 1) * sizeof(char));
+
+    partition(str, 0, length, path, 0);
+    free(path);
+}
+
+int main(void){
+    char input[100] = "NAVY";
+    //scanf("%s", input);
+    
+    subsequnces(input);
+    return 0;
+}
