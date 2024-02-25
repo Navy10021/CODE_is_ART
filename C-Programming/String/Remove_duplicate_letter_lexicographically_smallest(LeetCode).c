0b96554a8d5df6b3246e28e9c0b44524d56dfece
Yoonseop Lee <navy10021@snu.ac.kr>
2024-02-25 21:10:43 +0900

    Update Remove_duplicate_letter_lexicographically_smallest(LeetCode).c

--- C-Programming/String/Remove_duplicate_letter_lexicographically_smallest(LeetCode).c
+++ C-Programming/String/Remove_duplicate_letter_lexicographically_smallest(LeetCode).c
@@ -3,6 +3,7 @@
 #include<stdlib.h>
 
 // 316. Remove Duplicate Letters : return the smallest in lexicographical order among all possible results.
+// 중복문자를 제거하되, 사전순으로 정렬된 결과 리턴. "bcabc" -> "abc" / "cbadcbc" -> "acbc"
 void removeDuplicates(char * str){
     int seen[256] = {0};
     int newIdx = 0;
@@ -39,6 +40,7 @@ void genSubseq(char *str, int index, char *subseq, int* maxLen, char* resSubstr)
 }
 
 void RemoveDuplicateLetters(char * str){
+    // 1. 중복 제거된 길이(Max lenght)를 구한다.
     char * newStr = strdup(str);
     removeDuplicates(newStr);
     int maxLen = strlen(newStr);
@@ -47,7 +49,7 @@ void RemoveDuplicateLetters(char * str){
     char * subseq = (char*)malloc(sizeof(char)*(strLen+1));
     subseq[0] = '\0';
 
-    
+    // 2. Subsequnce를 생성하며, 중복제거시 Max length이면서 사전보다 빠르면 
     genSubseq(str, 0, subseq, &maxLen, maxSubstr);
     
     printf("Remove Duplicate Letters : %s\n", maxSubstr);
