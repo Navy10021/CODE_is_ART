e9ce17b36adce94fefa50fdd4ade5de51087ffe7
Yoonseop Lee <navy10021@snu.ac.kr>
2024-03-01 13:06:31 +0900

    Update LinkedList_Palindrome.py

--- Python-Programming/LinkedList/LinkedList_Palindrome.py
+++ Python-Programming/LinkedList/LinkedList_Palindrome.py
@@ -1,73 +1,86 @@
 class Node:
-    def __init__(self, data = 0, next = None):
-        self.data = data 
-        self.next = next
+    def __init__(self, val=None):
+        self.val = val
+        self.next = None
 
 class LinkedList:
     def __init__(self):
         self.head = None
 
-    def is_empty(self):
-        return self.head is None
-
-    def append(self, data):
-        new_node = Node(data)
-        if self.is_empty():
-            self.head = new_node
-            return
-        last_node = self.head
-        while last_node.next:
-            last_node = last_node.next
-        last_node.next = new_node
-
+    def append(self, val):
+        newNode = Node(val)
+        if not self.head:
+            self.head = newNode
+        else:
+            current = self.head
+            while current.next:
+                current = current.next
+            current.next = newNode
 
     def display(self):
         elements = []
-        current_node = self.head
-        while current_node:
-            elements.append(current_node.data)
-            current_node = current_node.next
+        current = self.head
+        while current:
+            elements.append(current.val)
+            current = current.next
         print(" -> ".join(map(str, elements)))
 
-def arrToList(arr):
-    ll = LinkedList()
-    for e in arr:
-        ll.append(e)
-    return ll 
-    
-# Palindrome Linked List 
-def isPalindrome(head):
-    # Find middle
-    slow = fast = head 
-    while fast and fast.next :
-        slow = slow.next 
-        fast = fast.next.next
-    
-    # Reverse second half 
-    second_half = None 
-    while slow:
-        temp = slow.next 
-        slow.next = second_half
-        second_half = slow
-        slow = temp 
-    
-    # Compare first and second 
-    first_half = head 
-    while second_half:
-        if first_half.data != second_half.data:
-            return False 
-        first_half = first_half.next 
-        second_half = second_half.next 
-    
-    return True
+    def is_palindrome(self):
+        if not self.head or not self.head.next:
+            return True
 
-L = ['A','B','C','B','A']
-LL = arrToList(L)
-LL.display()
+        # Find the middle
+        slow = fast = self.head
+        prev_of_slow = None
+        while fast and fast.next:
+            fast = fast.next.next
+            prev_of_slow = slow
+            slow = slow.next
 
-if (isPalindrome(LL.head)):
-    print("This Linked List is Palindrome")
-else:
-    print("No Palindrome")
+        midnode = None
 
+        # Handling odd size list
+        if fast:
+            midnode = slow
+            slow = slow.next
 
+        second_half_start = self.reverse_list(slow)
+        palindrome = self.compare_lists(self.head, second_half_start)
+
+        # Restore the original list (Reverse the second half back)
+        self.reverse_list(second_half_start)
+
+        if midnode:
+            prev_of_slow.next = midnode
+            midnode.next = slow
+        else:
+            prev_of_slow.next = slow
+
+        return palindrome
+
+    def reverse_list(self, head):
+        prev = None
+        current = head
+        while current:
+            next_node = current.next
+            current.next = prev
+            prev = current
+            current = next_node
+        return prev
+
+    def compare_lists(self, head1, head2):
+        while head2:  # Only need to compare till the end of second half or shorter list
+            if head1.val != head2.val:
+                return False
+            head1 = head1.next
+            head2 = head2.next
+        return True
+
+input = "radar"
+LL = LinkedList()
+for char in input:
+    LL.append(char)
+
+if(LL.is_palindrome()):
+    LL.display()
+    print("Is palindrome Linked List.")
