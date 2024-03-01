fc45159b2db37e89dcc7f696fbfe13d688551c4d
Yoonseop Lee <navy10021@snu.ac.kr>
2024-03-01 12:29:50 +0900

    Update LinkedList_Substring-II(no_adj).py

--- Python-Programming/LinkedList/LinkedList_Substring-II(no_adj).py
+++ Python-Programming/LinkedList/LinkedList_Substring-II(no_adj).py
@@ -1,21 +1,21 @@
 class Node:
-    def __init__(self, val = None):
-       self.val = val
-       self.next = None 
+    def __init__(self, val=None):
+        self.val = val
+        self.next = None
         
 class LinkedList:
     def __init__(self):
-        self.head = None 
+        self.head = None
     
     def is_empty(self):
-        return self.head is None 
+        return self.head is None
   
     def append(self, val):
         newNode = Node(val)
         if self.is_empty():
             self.head = newNode
-            return 
-        currNode = self.head 
+            return
+        currNode = self.head
         while currNode.next:
             currNode = currNode.next
         currNode.next = newNode
@@ -26,32 +26,30 @@ class LinkedList:
     
     def display(self):
         elements = []
-        currNode = self.head 
+        currNode = self.head
         while currNode:
             elements.append(currNode.val)
             currNode = currNode.next
         print(" -> ".join(map(str, elements)))
 
-
 def all_substrings(head):
     result = []
-    
-    # Traverse each Node
-    currNode = head 
-    while currNode:
-        # Generate substring start from Current Node
-        result.append(copy_linked_list(currNode))
-        currNode = currNode.next 
-        
+    start = head
+    while start:
+        temp = start
+        while temp:
+            result.append(copy_linked_list(start, temp))
+            temp = temp.next
+        start = start.next
     return result
 
-# Help function(Copy)
-def copy_linked_list(head):
+# Adjusted copy function to copy up to a certain node
+def copy_linked_list(start, end):
     copy_list = LinkedList()
-    currNode = head 
-    while currNode:
+    currNode = start
+    while currNode != end.next and currNode is not None:
         copy_list.append(currNode.val)
-        currNode = currNode.next 
+        currNode = currNode.next
     return copy_list
 
 # Special Conditions-I
@@ -66,13 +64,11 @@ def is_adj(linkedList):
         slow = slow.next 
     return False 
 
-input = "banana";
+input_str = "banana"
 LL = LinkedList()
-LL.strToList(input)
-#LL.display()
+LL.strToList(input_str)
 
 subLists = all_substrings(LL.head)
-# Special condition
 for ll in subLists:
     if not is_adj(ll):
         ll.display()
